fce4def99515a6cef04aa9407323f5cef2c3a683
Chuckpy <vynny_marcus@hotmail.com>
2021-12-21 09:26:29 -0300

    update

--- README.md
+++ README.md
@@ -1,17 +1,31 @@
-# SOM_suspicious-credit-activity
+# Análise de Crédito Suspeitas
 
+## Objetivo
 
-## Requirements
+Esse projeto foi feito como parte de estudos para análise de dados usando Python e bibliotecas especificas a análise. Nesse caso foi usado o dataset do Kagle como dados para teste, com dados de usuarios anonimos, valores(float) na conta, idade do usuário,emprestimo solicitado e efetivação do emprestimo.
 
-Install requirements.txt file to make sure correct versions of libraries are being used.
+## Rodando o projeto
 
-* Python 3.5.x
+Para inicializar o projeto, você precisa que todas as libs necessárias sejam devidamente instaladas, para isso você pode, dentro da pasta que clonou o projeto, instalar todas com o código :
+
+```bash
+pip install -r requirements.txt
+```
+
+Feito isso, você pode inicializar com o Python :
+
+```bash
+python3 credito.py
+```
+
+Se o projeto fez tudo corretamente, ele deve apresentar uma lista de suspeitos de maneira espaçada, porém é importante que você analise todos os dados que não consigo expor no terminal. Pra isso veja o código e rode dentro de uma ide pronta pra isso, como o Jupyter ou Spyder.
+
+![MiniSOM image](/minisom.png)
+
+## Requisitos
+
+* Python 3.5.x +
 * TensorFlow 1.10.0
 * Numpy 1.15.0
 * Keras 2.2.2
 * Matplotlib 2.2.2
-
-
-Aqui com um dataset do kagle, o desafio era usar um mapa auto organizavel para determinar quais são os id's de pessoas mais suspeitas
-
-
--- credito.py
+++ credito.py
@@ -1,52 +1,84 @@
 from minisom import MiniSom
 import pandas as pd 
 import numpy as np
+import os
 
+# change current working directory
+CURR_DIR = os.getcwd()
+
+os.chdir(CURR_DIR)
+# current working directory
+
+# setting the actual csv file to be compilated
 base = pd.read_csv("credit_data.csv")
 base = base.dropna()
 base.loc[base.age < 0 , "age"] = 40.92
 
-X=base.iloc[:, 0:4].values
-y= base.iloc [:,4].values
+# slicing result values from data
+X = base.iloc[:, 0:4].values
+Y = base.iloc[:,4].values
 
+# data normalization to minimize process stress
 from sklearn.preprocessing import MinMaxScaler
+
 normalizador = MinMaxScaler(feature_range = (0,1))
 X =normalizador.fit_transform(X)
-#criando efetivamente o mapa
-som = MiniSom(x=15 , y =15, input_len= 4, random_seed = 0)
-som.random_weights_init(X)
-som.train_random(data = X, num_iteration = 100)
 
-from pylab import pcolor, colorbar, plot
+# creating the map from here :
+som = MiniSom(x=15, y=15, input_len=4, random_seed=0)
+som.random_weights_init(X)
+som.train_random(data = X, num_iteration = 1000)
+
+# creating module visualization
+from pylab import pcolor, colorbar, plot  
 
 pcolor(som.distance_map().T)
 colorbar()
 
-markers = ["o", "s"]
-color = ["r", "g"]
-#marcando possiveis suspeitos dentro do registro de mapa
-for i, x in enumerate (X):
-    w= som.winner(x)
-    plot(w[0] + 0.5, w[1] + 0.5, markers[y[i]],
-markerfacecolor = 'None', markersize = 10,
-markeredgecolor = color[y[i]], markeredgewidth = 2)
+markers = ['o', 's']
+color = ['r', 'g']
 
-#selecionando manualmente no mapa neuronios que possuiam fortes tendencias a suspeitos
-mapeamento = som.win_map(X)
-suspeitos = np.concatenate((mapeamento[(5,6)],mapeamento[(6,14)], 
-                            mapeamento [(8,7)],mapeamento[(7,14)]), axis = 0)
-suspeitos = normalizador.inverse_transform(suspeitos)
+#marking possible suspects within the map record
+for i,x in enumerate (X):
+    w = som.winner(x)
+    plot(w[0]+ 0.5, w[1]+0.5, markers[Y[i]],
+         markerfacecolor = 'None', markersize = 10,
+         markeredgecolor = color[Y[i]], 
+         markeredgewidth=0.3)    
+
+#manually selecting on the map neurons that had strong tendencies to suspects
+mapping = som.win_map(X)
+suspects = np.concatenate((mapping[(5,9)],mapping[(6,9)], mapping[(6,10)],
+                           mapping[(7,12)], mapping[(7,13)], mapping[(14,4)],
+                           mapping[(14,6)], mapping[(13,11)],
+                           mapping[(13,14)], mapping[(4,8)]), axis = 0)
+
+# undo normalization
+suspects = normalizador.inverse_transform(suspects)
 
 classe = []
-#concatenando dados da base com a nova base de possiveis suspeitos
-for i in range(len(base)):
-    for j in range(len(suspeitos)):
-        if base.iloc[i, 0] == int(round(suspeitos[j,0])):
+
+for i in range(len(base)) :
+    for j in range(len(suspects)) :
+        if base.iloc[i,0] == int(round(suspects[j,0])):
             classe.append(base.iloc[i,4])
-           
+# transforming list to a numpy element
 classe = np.asarray(classe)
-         
-suspeitos_final = np.column_stack((suspeitos, classe))
-#aqui estão os suspeitos listados em ordem
-#dos que são suspeitos e foram aceitos, até os que são suspeitos mas não tiveram emprestimos aceitos
-suspeitos_final = suspeitos_final[suspeitos_final[:,4].argsort()]
+
+# finding the suspects that got the loan
+suspects_end = np.column_stack((suspects, classe))
+
+# finally here are all the suspects listed by order
+# of those who are suspects and have been accepted, even those who are suspects but have not had accepted
+suspects_end = suspects_end[suspects_end[:, 4].argsort()]
+
+import time
+sus = 1
+for i in suspects_end:   
+    print('\n')
+    print("-"*100)       
+    print(f'Suspect {sus} \n') 
+    sus+=1
+    for j in i :
+        print(str(j.tolist())+"|",end='')
+        time.sleep(0.1) 
